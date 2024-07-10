ee7e02697aae4ea367a3c3ea16c46b72bff6e25b
VJ537 <159228514+VJ537@users.noreply.github.com>
2024-07-10 12:29:36 +0530

    Delete first-spring directory

--- first-spring/src/main/java/com/spring/userRepositary/UserRepositary.java
+++
@@ -1,9 +0,0 @@
-package com.spring.userRepositary;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.spring.userEntity.User;
-
-public interface UserRepositary extends CrudRepository<User, Long> {
-
-}
